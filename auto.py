b892eaf4e26ed813b0e24e25b69be8c2af124048
AntonVI225 <142783538+AntonVI225@users.noreply.github.com>
2023-08-24 13:23:28 +0200

    pp
    neee

--- auto.py
+++ auto.py
@@ -1,6 +1,23 @@
-import openpyxl as xl
+import openpyxl as transact
+from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
+wb = transact.load_workbook('transact.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a2']
-print(cell)
+cell = sheet.cell[1, 2]
+
+for row in range(2, sheet.max_row + 1): # diese loop  
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+val = Reference (sheet, min_row=2,max_row=sheet.max_row,min_col = 4, max_cpl = 4 )
+
+chart = BarChart()
+chart.add_data(val)
+sheet.add_chart(chart, 'g5')
+
+
+wb.save('transact2.xlsx')
+
